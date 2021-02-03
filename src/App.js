0f9aeea249c13df137aaf534ad0881e0f212626f
bootupdesigner <bootup.itinfo@gmail.com>
2021-02-02 20:33:30 -0500

    created facebook messenger button with link

--- src/App.js
+++ src/App.js
@@ -13,6 +13,14 @@ import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
 import Menu from './components/Menu';
 import BootUpDesigner from './components/BootUpDesigner';
+import Messenger from './components/Messenger';
+
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faFacebookMessenger } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab, faFacebookMessenger)
 
 function App() {
   return (
@@ -24,6 +32,7 @@ function App() {
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
       </Switch>
+      <Messenger className="fa-2x"/>
       <Footer />
     </Router>
   );
---
+++ src/components/Messenger.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Messenger = () => {
+    return(
+        <div ClassName="messenger">
+            <a href="https://www.m.me/bootupdesigner">
+<FontAwesomeIcon icon={['fab', 'facebook-messenger']} size="3x" pull="right" />
+</a>
+</div>
+    );
+};
+export default Messenger;
