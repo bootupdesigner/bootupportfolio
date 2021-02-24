3c95cb99476d2dce2590373cb2079598a0a2f5a2
bootupdesigner <bootup.itinfo@gmail.com>
2021-02-23 23:10:28 -0500

    completed business starup description

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -10,7 +10,10 @@ const Services = () => {
         <h1>Services</h1>
         <p>Web Services Make Your Business Easier</p>
       </div>
-
+      <div className="startups" style={{textAlign: "center", display: "block", width: "100%"}}>
+        <h3>Business Startups</h3>
+        <p style={{padding: "0px 10vw 0px 10vw"}}>Boot Up Web Professionaionals offers a number of services to help jump start you business or creative idea. We'll provide you with Limitied Liability Company filings (LLC), Employer ID Number(EIN), Vendors Tax ID, Vendors List, Logos, and a <i>WEBSITE!</i></p>
+      </div>
       <WebServices />
 
       <div className="servicelink">
